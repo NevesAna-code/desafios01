19057b8048bd728077f7a8f34b4be5fefd6d0657
Ana Neves <neves.analima@gmail.com>
2025-02-21 08:57:50 -0300

    projeto desafios

---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,13 @@
+//1º Desafio
+let listaGenerica = [];
+
+//2º Desafio
+let linguaugensDeProgramacao = ['java', 'Ruby', 'Golang'];
+linguaugensDeProgramacao.push('JavaScript', 'c', 'c++', 'kotlin', 'Python'); //3º Desafio
+console.log(linguaugensDeProgramacao);
+
+//4º Desafio
+let CriarLista = ['Ana', 'Roberta', 'Mom'];
+console.log(CriarLista[0]);
+console.log(CriarLista[1]);
+console.log(CriarLista[2]);
